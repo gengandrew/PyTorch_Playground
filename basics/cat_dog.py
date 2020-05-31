0a99deab62546dd0a1341df8803f4f1e2fd1784b
Andrew Geng <gengandrew88@gmail.com>
2020-05-30 22:42:44 -0500

    Basic nn for cats/dogs

--- basics/cat_dog.py
+++ basics/cat_dog.py
@@ -1,9 +1,47 @@
 import os
 import cv2
+import torch
 import numpy as np
+import torch.nn as nn
 from tqdm import tqdm
+import torch.optim as optim
 import matplotlib.pyplot as plt
+import torch.nn.functional as func
 
+class Neural_Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        x = torch.randn(50,50).view(-1, 1, 50, 50)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 512)
+        self.fc2 = nn.Linear(512, 2)
+
+    def convs(self, x):
+        x = func.max_pool2d(func.relu(self.conv1(x)), (2,2))
+        x = func.max_pool2d(func.relu(self.conv2(x)), (2,2))
+        x = func.max_pool2d(func.relu(self.conv3(x)), (2,2))
+
+        if self._to_linear == None:
+            self._to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
+
+        return x
+    
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)
+        x = func.relu(self.fc1(x))
+        x = self.fc2(x)
+        return func.softmax(x, dim=1)
+
+epochs = 3
+batch_size = 64
+testing_set_size = 2500
 is_training_set_built = True
 
 def build_training_set():
@@ -42,11 +80,72 @@ def load_training_set():
     training_set = np.load("./kaggle/Cat_Dog_Training_Set.npy", allow_pickle=True)
     return training_set
 
-training_set = []
+raw_training_set = []
 if is_training_set_built == False:
-    training_set = build_training_set()
+    raw_training_set = build_training_set()
 else:
-    training_set = load_training_set()
+    raw_training_set = load_training_set()
 
-plt.imshow(training_set[1][0])
-plt.show()
+Net = Neural_Net()
+optimizer = optim.Adam(Net.parameters(), lr=0.001)
+loss_func = nn.MSELoss()
+
+x = []
+y = []
+for each in raw_training_set:
+    x.append(each[0])
+    y.append(each[1])
+
+x = torch.Tensor(x).view(-1,50,50)
+y = torch.Tensor(y)
+x = x/255
+
+training_set_x = x[:-testing_set_size]
+training_set_y = y[:-testing_set_size]
+testing_set_x = x[-testing_set_size:]
+testing_set_y = y[-testing_set_size:]
+
+for epoch in range(epochs):
+    for i in tqdm(range(0, len(training_set_x), batch_size)):
+        batch_x = training_set_x[i:i+batch_size].view(-1, 1, 50, 50)
+        batch_y = training_set_y[i:i+batch_size]
+
+        Net.zero_grad()
+        output = Net(batch_x)
+        loss = loss_func(output, batch_y)
+        loss.backward()
+        optimizer.step()
+        
+    print(loss)
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for i in tqdm(range(0, len(training_set_x))):
+        x = training_set_x[i]
+        y = training_set_y[i]
+        
+        output = Net(x.view(-1, 1, 50, 50))
+        prediction = torch.argmax(output)
+        if prediction == torch.argmax(y):
+            correct += 1
+        
+        total += 1
+
+print("Training Set Accuracy: ", correct/total)
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for i in tqdm(range(0, len(testing_set_x))):
+        x = testing_set_x[i]
+        y = testing_set_y[i]
+        
+        output = Net(x.view(-1, 1, 50, 50))
+        prediction = torch.argmax(output)
+        if prediction == torch.argmax(y):
+            correct += 1
+        
+        total += 1
+
+print("Testing Set Accuracy: ", correct/total)
